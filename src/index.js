f5e5cdbe6c8e2ee707405ea8b5d455080dddabe6
gufilion <61474187+gufilion@users.noreply.github.com>
2021-01-08 10:31:36 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,12 @@ import './index.css';
 import App_EN from './App_EN';
 import * as serviceWorker from './serviceWorker';
 
-if (document.documentElement.lang === "fr"){
     ReactDOM.render(
         <React.StrictMode>
-          <App_FR />
+          <App_EN/>
         </React.StrictMode>,
         document.getElementById('root')
-      );
-}
-else {
-    ReactDOM.render(
-        <React.StrictMode>
-          <App_EN />
-        </React.StrictMode>,
-        document.getElementById('root')
-      );
-}
+      )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
