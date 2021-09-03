860fcc5d3f56532dc99e5ffa196ddf61e4722990
gufilion <61474187+gufilion@users.noreply.github.com>
2021-09-03 10:10:45 -0700

    Add files via upload

---
+++ docs/precache-manifest.13cda2cea7d06608564f3f21ef59f5f9.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd4ccedb56cc5f0d83e80c284bba931",
+    "url": "/bc-data-literacy-self-assessment/index.html"
+  },
+  {
+    "revision": "4a61a9ddb513e52f012b",
+    "url": "/bc-data-literacy-self-assessment/static/css/2.56d5619b.chunk.css"
+  },
+  {
+    "revision": "0085406cb2697c4065cf",
+    "url": "/bc-data-literacy-self-assessment/static/css/main.5fdace04.chunk.css"
+  },
+  {
+    "revision": "4a61a9ddb513e52f012b",
+    "url": "/bc-data-literacy-self-assessment/static/js/2.ac742f3e.chunk.js"
+  },
+  {
+    "revision": "3878f23212436d69d0b5fb631ea4a4e0",
+    "url": "/bc-data-literacy-self-assessment/static/js/2.ac742f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0085406cb2697c4065cf",
+    "url": "/bc-data-literacy-self-assessment/static/js/main.51a6d2f1.chunk.js"
+  },
+  {
+    "revision": "5d60ea11181d650e2cbf",
+    "url": "/bc-data-literacy-self-assessment/static/js/runtime-main.bfc6f979.js"
+  },
+  {
+    "revision": "f9388525a7d662098998dab80f42db37",
+    "url": "/bc-data-literacy-self-assessment/static/media/Advanced.f9388525.md"
+  },
+  {
+    "revision": "d59e2eca82794156d4dc11fbce7d51e7",
+    "url": "/bc-data-literacy-self-assessment/static/media/AdvancedCourses.d59e2eca.md"
+  },
+  {
+    "revision": "1cab0a4e57bc42cd35d96ba086a56401",
+    "url": "/bc-data-literacy-self-assessment/static/media/Analyst.1cab0a4e.md"
+  },
+  {
+    "revision": "40be2dc32bcea65475a96485fc82ae82",
+    "url": "/bc-data-literacy-self-assessment/static/media/Architect.40be2dc3.md"
+  },
+  {
+    "revision": "916b05d85efe7eb942436bf98db9c029",
+    "url": "/bc-data-literacy-self-assessment/static/media/AssessmentResults.916b05d8.md"
+  },
+  {
+    "revision": "96d8ef8d4c4f046df5bdb9529bc9ca1e",
+    "url": "/bc-data-literacy-self-assessment/static/media/BCDataLiteracy.96d8ef8d.png"
+  },
+  {
+    "revision": "b7bf47d004bb4a155a9e0edcba3ba621",
+    "url": "/bc-data-literacy-self-assessment/static/media/BusinessTranslator.b7bf47d0.md"
+  },
+  {
+    "revision": "c98a3b959c0677682698da9d6d8a5e25",
+    "url": "/bc-data-literacy-self-assessment/static/media/Capable.c98a3b95.md"
+  },
+  {
+    "revision": "cf4c692f4ac5ab15b6fd567166b1098b",
+    "url": "/bc-data-literacy-self-assessment/static/media/CapableCourses.cf4c692f.md"
+  },
+  {
+    "revision": "3da3e288a513a8ccf787e7573adb10c1",
+    "url": "/bc-data-literacy-self-assessment/static/media/DataGovernor.3da3e288.md"
+  },
+  {
+    "revision": "2f4ebeb839add45c8379aefb45e9bd2e",
+    "url": "/bc-data-literacy-self-assessment/static/media/DataLiteracyLevel.2f4ebeb8.md"
+  },
+  {
+    "revision": "3b6505cda07662d8e2ee430c4b06a873",
+    "url": "/bc-data-literacy-self-assessment/static/media/DataLiteracyLogo.3b6505cd.png"
+  },
+  {
+    "revision": "4056dbab77a1da6e48ae195efb46f856",
+    "url": "/bc-data-literacy-self-assessment/static/media/DataPersona.4056dbab.md"
+  },
+  {
+    "revision": "ab1617da61e7f6298ae935bc6b2971c7",
+    "url": "/bc-data-literacy-self-assessment/static/media/DataScientist.ab1617da.md"
+  },
+  {
+    "revision": "cec1e8ec6e8a5720104f42a678989a0e",
+    "url": "/bc-data-literacy-self-assessment/static/media/Foundational.cec1e8ec.md"
+  },
+  {
+    "revision": "955c51eaab04ba9b9ac13983f3fe479b",
+    "url": "/bc-data-literacy-self-assessment/static/media/FoundationalCourses.955c51ea.md"
+  },
+  {
+    "revision": "02856d1fcc3a07b0834f9bd0dd1aa404",
+    "url": "/bc-data-literacy-self-assessment/static/media/FrontLine.02856d1f.md"
+  },
+  {
+    "revision": "ca3031479c5a9a6b2c2a851a23990c41",
+    "url": "/bc-data-literacy-self-assessment/static/media/HowDataLiterateAreYou.ca303147.md"
+  },
+  {
+    "revision": "f28d73874303b4d1d06b43329ba38dc1",
+    "url": "/bc-data-literacy-self-assessment/static/media/Leading.f28d7387.md"
+  },
+  {
+    "revision": "537bf893217a4b3dbcc8284f6e7355b2",
+    "url": "/bc-data-literacy-self-assessment/static/media/LeadingCourses.537bf893.md"
+  },
+  {
+    "revision": "57eec7fb03603e00f0a5f9e894b2cb8d",
+    "url": "/bc-data-literacy-self-assessment/static/media/Purpose.57eec7fb.md"
+  },
+  {
+    "revision": "7c67b6d34e0a15d482a4f20b4200b840",
+    "url": "/bc-data-literacy-self-assessment/static/media/WhoShouldUseThisTool.7c67b6d3.md"
+  },
+  {
+    "revision": "cc7acf13c2dbe2cee488f3adac5397be",
+    "url": "/bc-data-literacy-self-assessment/static/media/bcgovlogo.cc7acf13.jpg"
+  },
+  {
+    "revision": "d344d357354ef0c082eacba050e2e2d0",
+    "url": "/bc-data-literacy-self-assessment/static/media/bg.d344d357.png"
+  },
+  {
+    "revision": "4b3cf954d81f0de56357cabeeedb4f25",
+    "url": "/bc-data-literacy-self-assessment/static/media/data_literacy_bcps.4b3cf954.png"
+  }
+]);
